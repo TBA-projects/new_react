47426c26c087245945e1fb1b92d1690f381bec9e
Boris <ybibatejib812808@gmail.com>
2020-03-17 21:38:47 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const element = <App />;
+const container = document.getElementById('root');
+
+ReactDOM.render( element, container );
+
